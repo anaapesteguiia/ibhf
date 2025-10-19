be6d69058df485c4b89b8398b10ef4fa835e279b
Ana María <anaapesteguiia@gmail.com>
2025-10-19 08:39:07 +0200

    Añadidos los productos de ejemplo para las recompensas

---
+++ js/products.js
@@ -0,0 +1,73 @@
+// Recompensas de ejemplo
+const productos = [
+    {
+        id: 1,
+        nombre: "Tarjeta Regalo: envío gratis",
+        descripcion: "Envío gratis en compras superiores a 25€",
+        categoria: "tarjetas-regalo",
+        puntos: 2500
+    },
+    {
+        id: 2,
+        nombre: "Auriculares Inalámbricos",
+        descripcion: "Auriculares Bluetooth con cancelación de ruido",
+        categoria: "tecnologia",
+        puntos: 5000
+    },
+    {
+        id: 3,
+        nombre: "Fin de Semana en Barcelona",
+        descripcion: "2 noches en hotel 4* con desayuno incluido",
+        categoria: "viajes",
+        puntos: 15000
+    },
+    {
+        id: 4,
+        nombre: "Suscripción a plataforma de Streaming 6 meses",
+        descripcion: "Disfruta de 6 meses Premium de una plataforma de Streaming",
+        categoria: "servicios",
+        puntos: 3000
+    },
+    {
+        id: 5,
+        nombre: "Curso de Cocina Online",
+        descripcion: "Acceso a curso premium de cocina durante 1 año",
+        categoria: "experiencias",
+        puntos: 4000
+    },
+    {
+        id: 6,
+        nombre: "Tarjeta Regalo de moda",
+        descripcion: "Tarjeta regalo de 50€ para usar en tiendas de ropa",
+        categoria: "tarjetas-regalo",
+        puntos: 5000
+    },
+    {
+        id: 7,
+        nombre: "Sorteo de un Smartwatch Deportivo",
+        descripcion: "Reloj inteligente con GPS y monitorización cardiaca",
+        categoria: "tecnologia",
+        puntos: 8000
+    },
+    {
+        id: 8,
+        nombre: "Escape Room Premium",
+        descripcion: "Experiencia para 4 personas en un escape room temático",
+        categoria: "experiencias",
+        puntos: 3500
+    },
+    {
+        id: 9,
+        nombre: "Suscripción servicio de música premium 1 año",
+        descripcion: "Disfruta de música sin anuncios durante 12 meses",
+        categoria: "servicios",
+        puntos: 4500
+    },
+    {
+        id: 10,
+        nombre: "Vale Descuento en moda",
+        descripcion: "Vale de 30€ de descuento en compras superiores a 60€",
+        categoria: "tarjetas-regalo",
+        puntos: 2000
+    }
+];
